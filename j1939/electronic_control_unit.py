juergenH87/python-can-j1939

commit 546e4e7786910ec494c35e357bebd6dd712528bb

j1939-22 (j1939 fd)
implemented so far:
- multi-pg send/receive with possibility to enter a time limit
- Broadcast Announce Message (BAM) send/receive with up to 4 concurrent sessions and up to 15300 bytes of data per session
- RTS/CTS (Destination Specific) send/receive with up to 8 concurrent sessions and up to 16777215 bytes of data per session

added j1939-22 transport protocol and multi-pg examples

the data-link-layer (j1939-21 or j1939-22) can be selected when creating the ECU instance.
the data link layer cannot be changed dynamically, because j1939 does not allow mixing of data link layers either.

j1939/name.py
@@ -1,51 +1,51 @@
 
 class Name:
     """The Name of one Controller Application.
-    
+
     The Name consists of 64 bit:
 
         1-bit Arbitrary Address Capable
         Indicate the capability to solve address conflicts.
-        Set to 1 if the device is Arbitrary Address Capable, set to 0 if 
+        Set to 1 if the device is Arbitrary Address Capable, set to 0 if
         it's Single Address Capable.
 
         3-bit Industry Group
         One of the predefined J1939 industry groups.
 
         4-bit Vehicle System Instance
-        Instance number of a vehicle system to distinguish two or more 
-        device with the same Vehicle System number in the same J1939 
+        Instance number of a vehicle system to distinguish two or more
+        device with the same Vehicle System number in the same J1939
         network.
         The first instance is assigned to the instance number 0.
 
         7-bit Vehicle System
-        A subcomponent of a vehicle, that includes one or more J1939 
+        A subcomponent of a vehicle, that includes one or more J1939
         segments and may be connected or disconnected from the vehicle.
-        A Vehicle System may be made of one or more functions. The Vehicle 
+        A Vehicle System may be made of one or more functions. The Vehicle
         System depends on the Industry Group definition.
 
         1-bit Reserved
         This field is reserved for future use by SAE.
 
         8-bit Function
-        One of the predefined J1939 functions. The same function value 
-        (upper 128 only) may mean different things for different Industry 
+        One of the predefined J1939 functions. The same function value
+        (upper 128 only) may mean different things for different Industry
         Groups or Vehicle Systems.
 
         5-bit Function Instance
-        Instance number of a function to distinguish two or more devices 
+        Instance number of a function to distinguish two or more devices
         with the same function number in the same J1939 network.
         The first instance is assigned to the instance number 0.
 
         3-bit ECU Instance
-        Identify the ECU instance if multiple ECUs are involved in 
+        Identify the ECU instance if multiple ECUs are involved in
         performing a single function. Normally set to 0.
 
         11-bit Manufacturer Code
         One of the predefined J1939 manufacturer codes.
 
         21-bit Identity Number
-        A unique number which identifies the particular device in a 
+        A unique number which identifies the particular device in a
         manufacturer specific way.
     """
 
@@ -61,50 +61,50 @@ class Name:
         """
         :param value:
             64-bit value the address should be extracted from
-        
+
         :param bytes:
             Array of 8 bytes containing the name object as binary representation.
 
         :param arbitrary_address_capable:
             1-bit Arbitrary Address Capable
             Indicate the capability to solve address conflicts.
-            Set to 1 if the device is Arbitrary Address Capable, set to 0 if 
+            Set to 1 if the device is Arbitrary Address Capable, set to 0 if
             it's Single Address Capable.
         :param industry_group:
             3-bit Industry Group
             One of the predefined J1939 industry groups.
         :param vehicle_system_instance:
             4-bit Vehicle System Instance
-            Instance number of a vehicle system to distinguish two or more 
-            device with the same Vehicle System number in the same J1939 
+            Instance number of a vehicle system to distinguish two or more
+            device with the same Vehicle System number in the same J1939
             network.
             The first instance is assigned to the instance number 0.
         :param vehicle_system:
             7-bit Vehicle System
-            A subcomponent of a vehicle, that includes one or more J1939 
+            A subcomponent of a vehicle, that includes one or more J1939
             segments and may be connected or disconnected from the vehicle.
-            A Vehicle System may be made of one or more functions. The Vehicle 
+            A Vehicle System may be made of one or more functions. The Vehicle
             System depends on the Industry Group definition.
         :param function:
             8-bit Function
-            One of the predefined J1939 functions. The same function value 
-            (upper 128 only) may mean different things for different Industry 
+            One of the predefined J1939 functions. The same function value
+            (upper 128 only) may mean different things for different Industry
             Groups or Vehicle Systems.
         :param function_instance:
             5-bit Function Instance
-            Instance number of a function to distinguish two or more devices 
+            Instance number of a function to distinguish two or more devices
             with the same function number in the same J1939 network.
             The first instance is assigned to the instance number 0.
         :param ecu_instance:
             3-bit ECU Instance
-            Identify the ECU instance if multiple ECUs are involved in 
+            Identify the ECU instance if multiple ECUs are involved in
             performing a single function. Normally set to 0.
         :param manufacturer_code:
             11-bit Manufacturer Code
             One of the predefined J1939 manufacturer codes.
         :param identity_number:
             21-bit Identity Number
-            A unique number which identifies the particular device in a 
+            A unique number which identifies the particular device in a
             manufacturer specific way.
         """
         if 'value' in kwargs:
@@ -127,7 +127,7 @@ class Name:
     @property
     def arbitrary_address_capable(self):
         return self.__arbitrary_address_capable
-        
+
     @arbitrary_address_capable.setter
     def arbitrary_address_capable(self, value):
         self.__arbitrary_address_capable = value
@@ -135,7 +135,7 @@ class Name:
     @property
     def industry_group(self):
         return self.__industry_group
-        
+
     @industry_group.setter
     def industry_group(self, value):
         self.__industry_group = value
@@ -143,7 +143,7 @@ class Name:
     @property
     def vehicle_system_instance(self):
         return self.__vehicle_system_instance
-        
+
     @vehicle_system_instance.setter
     def vehicle_system_instance(self, value):
         self.__vehicle_system_instance = value
@@ -151,7 +151,7 @@ class Name:
     @property
     def vehicle_system(self):
         return self.__vehicle_system
-        
+
     @vehicle_system.setter
     def vehicle_system(self, value):
         self.__vehicle_system = value
@@ -159,7 +159,7 @@ class Name:
     @property
     def reserved_bit(self):
         return self.__reserved_bit
-        
+
     @reserved_bit.setter
     def reserved_bit(self, value):
         self.__reserved_bit = value
@@ -167,7 +167,7 @@ class Name:
     @property
     def function(self):
         return self.__function
-        
+
     @function.setter
     def function(self, value):
         self.__function = value
@@ -175,7 +175,7 @@ class Name:
     @property
     def function_instance(self):
         return self.__function_instance
-        
+
     @function_instance.setter
     def function_instance(self, value):
         self.__function_instance = value
@@ -183,7 +183,7 @@ class Name:
     @property
     def ecu_instance(self):
         return self.__ecu_instance
-        
+
     @ecu_instance.setter
     def ecu_instance(self, value):
         self.__ecu_instance = value
@@ -191,7 +191,7 @@ class Name:
     @property
     def manufacturer_code(self):
         return self.__manufacturer_code
-        
+
     @manufacturer_code.setter
     def manufacturer_code(self, value):
         self.__manufacturer_code = value
@@ -199,7 +199,7 @@ class Name:
     @property
     def identity_number(self):
         return self.__identity_number
-        
+
     @identity_number.setter
     def identity_number(self, value):
         self.__identity_number = value
@@ -247,4 +247,4 @@ class Name:
 
     @bytes.setter
     def bytes(self, value):
-        self.value = int("".join("%.2X" % _byte for _byte in value), 16)        
+        self.value = int("".join("%.2X" % _byte for _byte in value), 16)

examples/j1939_22_multi_pg.py
@@ -1,0 +1,98 @@
+import logging
+import time
+import j1939
+
+
+logging.getLogger('j1939').setLevel(logging.DEBUG)
+logging.getLogger('can').setLevel(logging.DEBUG)
+
+
+def on_message(priority, pgn, sa, timestamp, data):
+    """Receive incoming messages from the bus
+
+    :param int priority:
+        Priority of the message
+    :param int pgn:
+        Parameter Group Number of the message
+    :param int sa:
+        Source Address of the message
+    :param int timestamp:
+        Timestamp of the message
+    :param bytearray data:
+        Data of the PDU
+    """
+    print("PGN {} length {}".format(pgn, len(data)), timestamp)
+
+
+def ca_timer_callback1(ca):
+    """Callback for sending messages
+
+    This callback is registered at the ECU timer event mechanism to be
+    executed every 500ms.
+
+    :param cookie:
+        A cookie registered at 'add_timer'. May be None.
+    """
+    # wait until we have our device_address
+    if ca.state != j1939.ControllerApplication.State.NORMAL:
+        # returning true keeps the timer event active
+        return True
+
+    # create data with 20 bytes
+    data = [j1939.ControllerApplication.FieldValue.NOT_AVAILABLE_8] * 20
+
+    # sending broadcast message
+    # the following two PGNs are packed into one multi-pg, due to time-limit of 10ms and same destination address (global)
+    ca.send_pgn(0, 0xFD, 0xED, 6, data, time_limit=0.01)
+    ca.send_pgn(0, 0xFE, 0x32, 6, data, time_limit=0.01)
+
+    # sending normal peer-to-peer message, destintion address is 0x04
+    # the following PGNs are transferred separately, because time limit == 0
+    ca.send_pgn(0, 0xE0, 0x04, 6, data)
+    ca.send_pgn(0, 0xD0, 0x04, 6, data)
+
+    # returning true keeps the timer event active
+    return True
+
+
+def main():
+    print("Initializing")
+
+    # create the ElectronicControlUnit (one ECU can hold multiple ControllerApplications) with j1939-22 data link layer
+    ecu = j1939.ElectronicControlUnit(data_link_layer='j1939-22', max_cmdt_packets=200)
+
+    # can fd Baud: 500k/2M
+    ecu.connect(bustype='pcan', channel='PCAN_USBBUS1', fd=True,
+                        f_clock_mhz=80, nom_brp=10, nom_tseg1=12, nom_tseg2=3, nom_sjw=1, data_brp=4, data_tseg1=7, data_tseg2=2, data_sjw=1)
+
+    # subscribe to all (global) messages on the bus
+    ecu.subscribe(on_message)
+
+    # compose the name descriptor for the new ca
+    name = j1939.Name(
+        arbitrary_address_capable=0,
+        industry_group=j1939.Name.IndustryGroup.Industrial,
+        vehicle_system_instance=1,
+        vehicle_system=1,
+        function=1,
+        function_instance=1,
+        ecu_instance=1,
+        manufacturer_code=666,
+        identity_number=1234567
+        )
+
+    # create the ControllerApplications
+    ca = j1939.ControllerApplication(name, 0x1)
+    ecu.add_ca(controller_application=ca)
+    # callback every 0.5s
+    ca.add_timer(0.500, ca_timer_callback1, ca)
+    ca.start()
+
+    time.sleep(120)
+
+    print("Deinitializing")
+    ca.stop()
+    ecu.disconnect()
+
+if __name__ == '__main__':
+    main()

examples/j1939_22_transport_protocols.py
@@ -1,0 +1,98 @@
+import logging
+import time
+import j1939
+
+
+logging.getLogger('j1939').setLevel(logging.DEBUG)
+logging.getLogger('can').setLevel(logging.DEBUG)
+
+
+def on_message(priority, pgn, sa, timestamp, data):
+    """Receive incoming messages from the bus
+
+    :param int priority:
+        Priority of the message
+    :param int pgn:
+        Parameter Group Number of the message
+    :param int sa:
+        Source Address of the message
+    :param int timestamp:
+        Timestamp of the message
+    :param bytearray data:
+        Data of the PDU
+    """
+    print("PGN {} length {}".format(pgn, len(data)), timestamp)
+
+
+def ca_timer_callback1(ca):
+    """Callback for sending messages
+
+    This callback is registered at the ECU timer event mechanism to be
+    executed every 500ms.
+
+    :param cookie:
+        A cookie registered at 'add_timer'. May be None.
+    """
+    # wait until we have our device_address
+    if ca.state != j1939.ControllerApplication.State.NORMAL:
+        # returning true keeps the timer event active
+        return True
+
+    # create data with 500 bytes
+    data = [j1939.ControllerApplication.FieldValue.NOT_AVAILABLE_8] * 500
+
+    # sending transport protocol broadcast message (BAM)
+    successful = ca.send_pgn(0, 0xFD, 0xED, 6, data)
+    if not successful:
+        print( 'error occurred while sending BAM' )
+
+    # sending transport protocol peer-to-peer message (rts/cts), destintion address is 0x04
+    successful = ca.send_pgn(0, 0xE0, 0x04, 6, data)
+    if not successful:
+        print( 'error occurred while sending rts/cts message' )
+
+    # returning true keeps the timer event active
+    return True
+
+
+def main():
+    print("Initializing")
+
+    # create the ElectronicControlUnit (one ECU can hold multiple ControllerApplications) with j1939-22 data link layer
+    ecu = j1939.ElectronicControlUnit(data_link_layer='j1939-22', max_cmdt_packets=200)
+
+    # can fd Baud: 500k/2M
+    ecu.connect(bustype='pcan', channel='PCAN_USBBUS1', fd=True,
+                        f_clock_mhz=80, nom_brp=10, nom_tseg1=12, nom_tseg2=3, nom_sjw=1, data_brp=4, data_tseg1=7, data_tseg2=2, data_sjw=1)
+
+    # subscribe to all (global) messages on the bus
+    ecu.subscribe(on_message)
+
+    # compose the name descriptor for the new ca
+    name = j1939.Name(
+        arbitrary_address_capable=0,
+        industry_group=j1939.Name.IndustryGroup.Industrial,
+        vehicle_system_instance=1,
+        vehicle_system=1,
+        function=1,
+        function_instance=1,
+        ecu_instance=1,
+        manufacturer_code=666,
+        identity_number=1234567
+        )
+
+    # create the ControllerApplications
+    ca = j1939.ControllerApplication(name, 0x1)
+    ecu.add_ca(controller_application=ca)
+    # callback every 0.5s
+    ca.add_timer(0.500, ca_timer_callback1, ca)
+    ca.start()
+
+    time.sleep(120)
+
+    print("Deinitializing")
+    ca.stop()
+    ecu.disconnect()
+
+if __name__ == '__main__':
+    main()

commit 57c40e5ec0b48e673b7f251f94f77fd911907fee

Fix broken tests (#13)
* Fix address claiming endianness

* Skip sending abort when received abort before CTS

* update test data to match change in 8675eaa

#12

* restore ability to override ElectronicControlUnit.send_message

(broken in 546e4e7)
#12

* adjust vehicle_system value in tests to comply with length limit

#12

* change expected data to account for change in ddf4907

#12

* change expected data to match default max_cmdt_packets

#12

Co-authored-by: FilipZZZ <filipzvonar@gmail.com>

j1939/electronic_control_unit.py
@@ -16,11 +16,14 @@ class ElectronicControlUnit:
     """ElectronicControlUnit (ECU) holding one or more ControllerApplications (CAs)."""
 
 
-    def __init__(self, data_link_layer='j1939-21', max_cmdt_packets=1, minimum_tp_rts_cts_dt_interval=None, minimum_tp_bam_dt_interval=None):
+    def __init__(self, data_link_layer='j1939-21', max_cmdt_packets=1, minimum_tp_rts_cts_dt_interval=None, minimum_tp_bam_dt_interval=None, send_message=None):
         """
         :param data_link_layer:
             specify data-link-layer, 'j1939-21' or 'j1939-22'
         """
+        if send_message:
+            self.send_message = send_message
+
         #: A python-can :class:`can.BusABC` instance
         self._bus = None
         # Locking object for send

j1939/j1939_21.py
@@ -350,7 +350,11 @@ class J1939_21:
                 }
             self.__job_thread_wakeup()
         elif control_byte == self.ConnectionMode.ABORT:
-            # TODO
+            # if abort received before transmission established -> cancel transmission 
+            buffer_hash = self._buffer_hash(dest_address, src_address)
+            if buffer_hash in self._snd_buffer and self._snd_buffer[buffer_hash]['state'] == self.SendBufferState.WAITING_CTS:
+                del self._snd_buffer[buffer_hash] # cancel transmission 
+            # TODO: any more abort responses?
             pass
         else:
             raise RuntimeError("Received TP.CM with unknown control_byte %d", control_byte)

j1939/name.py
@@ -266,4 +266,4 @@ class Name:
 
     @bytes.setter
     def bytes(self, value):
-        self.value = int("".join("%.2X" % _byte for _byte in value), 16)
+        self.value = int.from_bytes(value, byteorder='little', signed=False)

test/test_ca.py
@@ -50,7 +50,7 @@ class TestCA(unittest.TestCase):
         expected_data = self.can_messages.pop(0)
         self.assertEqual(expected_data[0], TestCA.MsgType.CANTX, "No transmission was expected")
         self.assertEqual(can_id, expected_data[1])
-        self.assertSequenceEqual(data[::-1], expected_data[2])
+        self.assertSequenceEqual(data, expected_data[2])
         self._inject_messages_into_ecu()
 
     def _on_message(self, pgn, data):
@@ -64,7 +64,7 @@ class TestCA(unittest.TestCase):
         expected_data = self.pdus.pop(0)
         self.assertEqual(expected_data[0], TestCA.MsgType.PDU)
         self.assertEqual(pgn, expected_data[1])
-        self.assertSequenceEqual(data[::-1], expected_data[2])
+        self.assertSequenceEqual(data, expected_data[2])
 
     def setUp(self):
         """Called before each test methode.
@@ -80,10 +80,8 @@ class TestCA(unittest.TestCase):
         self.message_queue = queue.Queue()
         self.message_thread = threading.Thread(target=self._async_can_feeder)
         self.message_thread.start()
-        
-        self.ecu = j1939.ElectronicControlUnit()
         # redirect the send_message from the can bus to our simulation
-        self.ecu.send_message = self._send_message
+        self.ecu = j1939.ElectronicControlUnit(send_message=self._send_message)
 
     def tearDown(self):
         """Called after each test methode.
@@ -107,14 +105,14 @@ class TestCA(unittest.TestCase):
         address of 128.
         """
         self.can_messages = [
-            (TestCA.MsgType.CANTX, 0x18EEFF80, [83, 54, 201, 130, 83, 82, 214, 135], 0.0),    # Address Claimed
+            (TestCA.MsgType.CANTX, 0x18EEFF80, [135, 214, 82, 83, 130, 201, 254, 82], 0.0),    # Address Claimed
         ]
 
         name = j1939.Name(
             arbitrary_address_capable=0, 
             industry_group=j1939.Name.IndustryGroup.Industrial,
             vehicle_system_instance=2,
-            vehicle_system=155,
+            vehicle_system=127,
             function=201,
             function_instance=16,
             ecu_instance=2,
@@ -141,16 +139,16 @@ class TestCA(unittest.TestCase):
         with a veto and we lose our address.
         """
         self.can_messages = [
-            (TestCA.MsgType.CANTX, 0x18EEFF80, [83, 54, 201, 130, 83, 82, 214, 135], 0.0),    # Address Claimed
-            (TestCA.MsgType.CANRX, 0x18EEFF80, [83, 54, 111, 130, 83, 82, 214, 135], 0.0),    # Veto from Counterpart with lower name
-            (TestCA.MsgType.CANTX, 0x18EEFFFE, [83, 54, 201, 130, 83, 82, 214, 135], 0.0),    # CANNOT CLAIM
+            (TestCA.MsgType.CANTX, 0x18EEFF80, [135, 214, 82, 83, 130, 201, 254, 82], 0.0),    # Address Claimed
+            (TestCA.MsgType.CANRX, 0x18EEFF80, [135, 214, 82, 83, 130, 111, 254, 82], 0.0),    # Veto from Counterpart with lower name
+            (TestCA.MsgType.CANTX, 0x18EEFFFE, [135, 214, 82, 83, 130, 201, 254, 82], 0.0),    # CANNOT CLAIM
         ]
 
         name = j1939.Name(
             arbitrary_address_capable=0, 
             industry_group=j1939.Name.IndustryGroup.Industrial,
             vehicle_system_instance=2,
-            vehicle_system=155,
+            vehicle_system=127,
             function=201,
             function_instance=16,
             ecu_instance=2,
@@ -177,16 +175,16 @@ class TestCA(unittest.TestCase):
         with a veto, but our name is less.
         """
         self.can_messages = [
-            (TestCA.MsgType.CANTX, 0x18EEFF80, [83, 54, 201, 130, 83, 82, 214, 135], 0.0),    # Address Claimed
-            (TestCA.MsgType.CANRX, 0x18EEFF80, [83, 54, 222, 130, 83, 82, 214, 135], 0.0),    # Veto from Counterpart with higher name
-            (TestCA.MsgType.CANTX, 0x18EEFF80, [83, 54, 201, 130, 83, 82, 214, 135], 0.0),    # resend Address Claimed
+            (TestCA.MsgType.CANTX, 0x18EEFF80, [135, 214, 82, 83, 130, 201, 254, 82], 0.0),    # Address Claimed
+            (TestCA.MsgType.CANRX, 0x18EEFF80, [135, 214, 82, 83, 130, 222, 254, 82], 0.0),    # Veto from Counterpart with higher name
+            (TestCA.MsgType.CANTX, 0x18EEFF80, [135, 214, 82, 83, 130, 201, 254, 82], 0.0),    # resend Address Claimed
         ]
 
         name = j1939.Name(
             arbitrary_address_capable=0, 
             industry_group=j1939.Name.IndustryGroup.Industrial,
             vehicle_system_instance=2,
-            vehicle_system=155,
+            vehicle_system=127,
             function=201,
             function_instance=16,
             ecu_instance=2,
@@ -214,16 +212,16 @@ class TestCA(unittest.TestCase):
         (129) automatically.
         """
         self.can_messages = [
-            (TestCA.MsgType.CANTX, 0x18EEFF80, [211, 54, 201, 130, 83, 82, 214, 135], 0.0),    # Address Claimed 128
-            (TestCA.MsgType.CANRX, 0x18EEFF80, [83, 54, 111, 130, 83, 82, 214, 135], 0.0),     # Veto from Counterpart with lower name
-            (TestCA.MsgType.CANTX, 0x18EEFF81, [211, 54, 201, 130, 83, 82, 214, 135], 0.0),    # Address Claimed 129
+            (TestCA.MsgType.CANTX, 0x18EEFF80, [135, 214, 82, 83, 130, 201, 254, 210], 0.0),    # Address Claimed 128
+            (TestCA.MsgType.CANRX, 0x18EEFF80, [135, 214, 82, 83, 130, 111, 254, 82], 0.0),     # Veto from Counterpart with lower name
+            (TestCA.MsgType.CANTX, 0x18EEFF81, [135, 214, 82, 83, 130, 201, 254, 210], 0.0),    # Address Claimed 129
         ]
 
         name = j1939.Name(
             arbitrary_address_capable=1, 
             industry_group=j1939.Name.IndustryGroup.Industrial,
             vehicle_system_instance=2,
-            vehicle_system=155,
+            vehicle_system=127,
             function=201,
             function_instance=16,
             ecu_instance=2,

test/test_ecu.py
@@ -104,10 +104,8 @@ class TestECU(unittest.TestCase):
         self.message_queue = queue.Queue()
         self.message_thread = threading.Thread(target=self._async_can_feeder)
         self.message_thread.start()
-        
-        self.ecu = j1939.ElectronicControlUnit()
         # redirect the send_message from the can bus to our simulation
-        self.ecu.send_message = self._send_message
+        self.ecu = j1939.ElectronicControlUnit(send_message=self._send_message)
         # install a fake-CA to accept all messages
         ca = AcceptAllCA(None)
         self.ecu.add_ca(controller_application = ca)
@@ -268,7 +266,7 @@ class TestECU(unittest.TestCase):
         Its length is 20 Bytes.
         """
         self.can_messages = [
-            (TestECU.MsgType.CANTX, 0x18EC9B90, [16, 20, 0, 3, 255, 0, 223, 0], 0.0),        # TP.CM RTS 1
+            (TestECU.MsgType.CANTX, 0x18EC9B90, [16, 20, 0, 3, 1, 0, 223, 0], 0.0),          # TP.CM RTS 1
             (TestECU.MsgType.CANRX, 0x1CEC909B, [17, 1, 1, 255, 255, 0, 223, 0], 0.0),       # TP.CM CTS 1
             (TestECU.MsgType.CANTX, 0x1CEB9B90, [1, 1, 2, 3, 4, 5, 6, 7], 0.0),              # TP.DT 1
             (TestECU.MsgType.CANRX, 0x1CEC909B, [17, 1, 2, 255, 255, 0, 223, 0], 0.0),       # TP.CM CTS 2