88cda38483eebf2653af6fb5a249db512e1e33a5
Koltan Hauersperger <Khauersperger@jlg.com>
2023-10-27 10:10:45 -0400

    chore: remove return hints

--- j1939/memory_access.py
+++ j1939/memory_access.py
@@ -3,7 +3,6 @@ import queue
 import sys
 import time
 import secrets
-from typing import Union
 import j1939
 
 
@@ -455,7 +454,7 @@ class DM14Response:
         error: int = 0xFFFF,
         seed_override: bool = False,
         seed: int = 0x01,
-    ) -> int | None:
+    ):
         """
         Respond to DM14 query with the requested data or confimation of operation is good to proceed
         :param bool proceed: whether the operation is good to proceed
